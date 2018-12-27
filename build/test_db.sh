0a3e897e45e9b7c126845662339d57041cac92ba
Arba Sasmoyo <arba.sasmoyo@gmail.com>
2018-12-27 20:05:17 +0700

    no need to store stucture.sql

--- build/test_db.sh
+++ build/test_db.sh
@@ -5,10 +5,5 @@ rake db:drop
 rake db:create
 rake db:migrate
 
-# make sure structure.sql is updated
-cp db/structure.sql db/structure.sql.old
-rake db:structure:dump
-diff db/structure.sql db/structure.sql.old
-
 # make sure reverting migrations is working
 rake db:migrate VERSION=0
